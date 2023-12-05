5135d44d19b1f85f2aa653f096fd520717034775
domy0001 <domenicolagana18@gmail.com>
2023-12-05 15:40:15 +0100

    last version

--- bedrock_call.py
+++ bedrock_call.py
@@ -3,37 +3,24 @@ import json
 import boto3
 import pickle
 import numpy as np
+import time
 import botocore
 import langchain
 from langchain.vectorstores import FAISS
 from langchain.embeddings import BedrockEmbeddings
 from botocore.config import Config
 
-print(f'Numpy version: {np.__version__}')
-print(f'Langchain version: {langchain.__version__}')
-print(f'Boto version: {boto3.__version__}')
-print(f'Botocore version: {botocore.__version__}')
+level = "[INFO] - "
+
+print(f'{level} Numpy version: {np.__version__}')
+print(f'{level} Langchain version: {langchain.__version__}')
+print(f'{level} Boto version: {boto3.__version__}')
+print(f'{level} Botocore version: {botocore.__version__}')
 
 bedrock_region = "us-east-1"
-bedrock_role = "arn:aws:iam::743456971407:role/BedrockCustomerPoc-CAAutoBank"
-bedrock_endpoint = None
-model_id = "amazon.titan-tg1-large"
-boto3_kwargs = {'endpoint_url': bedrock_endpoint}
-session_kwargs = {"region_name": bedrock_region}
-client_kwargs = {**session_kwargs}
 s3_client = boto3.client("s3")
 output_type = os.environ.get('RESPONSE_TYPE')
-session = boto3.Session(**session_kwargs)
-config = Config(
-    region_name=bedrock_region,
-    retries={
-        'max_attempts': 10,
-        'mode': 'standard'
-    }
-)
-
 s3 = boto3.resource('s3')
-
 obj = s3.Object("vect-db", f"context/{os.environ.get('CONTEXT')}")
 rules_context = obj.get()['Body'].read().decode('utf-8') 
 
@@ -47,35 +34,10 @@ parameters = {
 accept = "application/json"
 content_type = "application/json"
 
-
-def get_client():
-    if bedrock_role:
-        print(f"  Using role: {bedrock_role}", end='')
-        sts = session.client("sts")
-        response = sts.assume_role(
-            RoleArn=str(bedrock_role),
-            RoleSessionName="langchain-llm-1"
-        )
-        print(" ... successful!")
-        client_kwargs["aws_access_key_id"] = response["Credentials"]["AccessKeyId"]
-        client_kwargs["aws_secret_access_key"] = response["Credentials"]["SecretAccessKey"]
-        client_kwargs["aws_session_token"] = response["Credentials"]["SessionToken"]
-
-    if bedrock_endpoint:
-        client_kwargs["endpoint_url"] = bedrock_endpoint
-
-    bedrock_client = session.client(
-        service_name="bedrock",
-        config=config,
-        **client_kwargs
-    )
-    return bedrock_client
-
-
-bedrock_client = get_client()
+bedrock_client = boto3.client('bedrock-runtime')
 
 def get_bedrock_embeddigns_model():
-    return BedrockEmbeddings(model_id="amazon.titan-embed-g1-text-02", client=bedrock_client)
+    return BedrockEmbeddings(model_id="amazon.titan-embed-text-v1", client=bedrock_client)
 
 def get_index_pkl():
     response = s3_client.get_object(
@@ -85,34 +47,42 @@ def get_index_pkl():
 
 index_string_data = get_index_pkl()
 
-def get_response(input_text, model_id, accept, content_type):
-    
-
+def get_response(input_text, accept, content_type):
+    doc_words_count = 0
     query = input_text
+    start_embedding = time.time()
     query_embedding = vectorstore_faiss.embedding_function(query)
     np.array(query_embedding)
+    end_embedding = time.time()
+    print(f'{level} Embedding input text execution time: {end_embedding-start_embedding}')
+    start_faiss = time.time()
     relevant_documents = vectorstore_faiss.similarity_search_by_vector(query_embedding)
-    print(f'{len(relevant_documents)} documents are fetched which are relevant to the query.')
-    print('----')
+    end_faiss = time.time()
+    print(f'{level} Faiss similarity_search_by_vector execution time: {end_faiss-start_faiss}')
     context = rules_context
     for i, rel_doc in enumerate(relevant_documents):
-        print(f'## Document {i+1}: {rel_doc.page_content}.......')
-        print('---')
+        doc_words_count += len(rel_doc.page_content)
         context = context + "\n" + "<doc>" + rel_doc.page_content + "</doc>"
     context += "</context>"
+    print(f'Documents token: {doc_words_count}')
+    print(f'Context tokens: {len(context.split())}')
     prompt_template = f"\n\nHuman: {context}\n Question: {query}\n\nAssistant:"
     body = dict()
     body['prompt'] = prompt_template
-    body['temperature'] = 0
-    body['top_p'] = 0.1
-    body['top_k'] = 4
-    body['max_tokens_to_sample'] = 10000
+    body['temperature'] = 0.6
+    body['top_p'] = 0.999
+    body['top_k'] = 250
+    body['max_tokens_to_sample'] = 2000
     body['stop_sequences'] = ["\n\nHuman:"]
+
+    start_bedrock = time.time()
     response = bedrock_client.invoke_model(body=json.dumps(body), modelId="anthropic.claude-v2", accept=accept,
                                            contentType=content_type)
+    end_bedrock = time.time()
+    print(f'{level} Bedrock invoke model execution time: {end_bedrock-start_bedrock}')                                        
     response_body = json.loads(response.get("body").read())
     answer = response_body.get("completion")
-    print(f'\n Answer: {answer}')
+    print(f'\n {level} Answer: {answer}')
     return answer
 
 bedrock_embeddings = get_bedrock_embeddigns_model()
@@ -121,14 +91,12 @@ vectorstore_faiss_pkl = pickle.loads(index_string_data)
 vectorstore_faiss = FAISS.deserialize_from_bytes(embeddings=bedrock_embeddings, serialized=vectorstore_faiss_pkl)
 
 def handler(event, context):
-    print(json.dumps(event))
-    print("input transcription:" + event['inputTranscript'])
+    print(f"{level} Input transcription:" + event['inputTranscript'])
     if len(event['inputTranscript']) == 0:
         content = "Mi dispiace, ma non ho compreso la sua domanda. Potrebbe essere più chiaro?"
     else:
         content = get_response(
             input_text=event['inputTranscript'],
-            model_id=model_id,
             accept=accept,
             content_type=content_type)
     response = {
@@ -149,7 +117,7 @@ def handler(event, context):
             }
         ]
     }
-    print(f'Response: {response}')
+    print(f'{level} Response: {response}')
     return response
 
 
